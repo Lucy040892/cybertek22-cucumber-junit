0c371ef885fddc50f829d2fd1557f80dfcf6e925
Lucy040892 <Komisarenkoliudmyla@gmail.com>
2021-07-25 10:29:33 -0500

    Cucumber Day6 Apache poi

--- src/test/java/com/cybertek/tests/ExcelRead.java
+++ src/test/java/com/cybertek/tests/ExcelRead.java
@@ -1,13 +1,30 @@
 package com.cybertek.tests;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class ExcelRead {
     /**
      * In this method we will learn how to read from excel file step by step
      */
     @Test
-    public void read_from_excel_file() {
+    public void read_from_excel_file() throws IOException {
+
+        String path = "SampleData.xlsx";
+
+        //To be able to read from Excel file, we need to load it into FileInputStream
+        FileInputStream fileInputStream = new FileInputStream(path);
+
+        //workbook > sheet > row > cell
+        //1- Create a workbook
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+
+        //2- We need to get the specific sheet from currently opened workbook
+        XSSFSheet sheet = workbook.getSheet("Employees");
 
     }
 
